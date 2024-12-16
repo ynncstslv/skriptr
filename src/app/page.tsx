f60c406c21165e981128409990bcac36e201a4c2
ynncstslv <ynncstslv@gmail.com>
2024-12-16 17:10:39 -0300

    remove clerk components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import {
-	SignedIn,
-	SignedOut,
-	SignOutButton,
-	SignUpButton,
-} from '@clerk/nextjs';
-
 export default function Home() {
-	return (
-		<div>
-			<SignedOut>
-				<SignUpButton />
-			</SignedOut>
-			<SignedIn>
-				<SignOutButton />
-			</SignedIn>
-		</div>
-	);
+	return <div></div>;
 }
