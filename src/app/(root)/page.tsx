8a69f3f79e6a544a98961d0a5e7212f8b8b1eee4
ynncstslv <ynncstslv@gmail.com>
2024-12-17 13:16:32 -0300

    fix: change home page root directory

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div className="min-h-screen">
+			<div className="max-w-[1800px] mx-auto p-4">
+				<div>header</div>
+				<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+					editor and output
+				</div>
+			</div>
+		</div>
+	);
+}
