37f09aebcc08e91fbd4627e0a5b929862d785a61
ynncstslv <ynncstslv@gmail.com>
2025-03-01 23:01:34 -0300

    feat: setup convex database

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: 'https://accepted-goat-91.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import ConvexClientProvider from '@/components/providers/ConvexClientProvider';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -31,7 +32,7 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				>
-					{children}
+					<ConvexClientProvider>{children}</ConvexClientProvider>
 				</body>
 			</html>
 		</ClerkProvider>
