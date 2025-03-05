9bcaeb8d740c5f971269140de85e490a8e87785e
ynncstslv <ynncstslv@gmail.com>
2025-03-05 11:16:05 -0300

    feat: create webhook from clerk to convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SignOutButton, SignUpButton } from '@clerk/nextjs';
+
 export default function Home() {
-	return <div></div>;
+	return (
+		<div>
+			<SignUpButton />
+			<SignOutButton />
+		</div>
+	);
 }
